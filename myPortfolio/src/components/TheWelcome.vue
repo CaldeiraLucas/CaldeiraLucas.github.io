527bb2ac918fba5c87ac275351e88a06ca9266cf
Lucas Caldeira <l.caldeira.m@gmail.com>
2022-03-14 15:37:37 -0300

    changing side icons

--- myPortfolio/src/components/TheWelcome.vue
+++ myPortfolio/src/components/TheWelcome.vue
@@ -2,25 +2,27 @@
 import WelcomeItens from "./WelcomeItens.vue";
 import WelcomeItem from "./WelcomeItem.vue";
 import ScrollSpy from "./ScrollSpy.vue";
-import DocumentationIcon from "./icons/IconDocumentation.vue";
+import WorksIcon from "./icons/IconWorks.vue";
 import ProfileIcon from "./icons/IconProfile.vue";
 import StudiesIcon from "./icons/IconStudies.vue";
-
+import SkillsIcon from "./icons/IconSkills.vue";
 </script>
 
 <template>
   <ScrollSpy>
     <template #icon1>
-      <ProfileIcon/>
+      <ProfileIcon />
     </template>
     <template #icon2>
       <StudiesIcon />
     </template>
     <template #icon3>
-      <DocumentationIcon />
+      <WorksIcon />
+    </template>
+    <template #icon4>
+      <SkillsIcon />
     </template>
   </ScrollSpy>
-  <WelcomeItens>
-  </WelcomeItens>
+  <WelcomeItens> </WelcomeItens>
 </template>
 
