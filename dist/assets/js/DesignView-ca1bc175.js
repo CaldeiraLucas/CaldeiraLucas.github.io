55f02f05854628e73fca0dd16e8d3dc17be00ecb
Lucas Caldeira <l.caldeira.m@gmail.com>
2022-06-23 23:29:49 -0300

    updating dist subtree

--- dist/assets/js/DesignView-ca1bc175.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./index-1a019a7c.js";import{o as s,c as n,a as t}from"./vendor-ca9dc44e.js";const _={},o={class:"design"},c=t("h1",null,"This is an page in construction",-1),a=[c];function i(r,d){return s(),n("div",o,a)}var p=e(_,[["render",i]]);export{p as default};
